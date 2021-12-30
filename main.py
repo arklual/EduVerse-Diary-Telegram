73bee8bacd513bcfe13d6eb6f52dc8b2c7751f5f
arklual <arklual1@gmail.com>
2021-12-30 12:51:29 +0300

    init

---
+++ main.py
@@ -0,0 +1,93 @@
+import requests
+from prettytable import PrettyTable
+from bs4 import BeautifulSoup
+
+def main():
+    username = input('Username: ')
+    password = input('Password: ')
+    print(get_table(parse_subjects(login(username=username, password=password))))
+
+def get_table(subjects):
+    t = PrettyTable()
+    max_marks = max([len(subjects[i]) for i in subjects])
+    t.field_names = ["Предмет", "Ср. балл"] + [f'Оценка {i+1}' for i in range(max_marks-1)]
+    for subject in subjects:
+        while len(subjects[subject]) < max_marks:
+            subjects[subject].append(' ')
+        t.add_row([subject]+subjects[subject])
+    return t
+
+def login(username, password):
+    cookies = {
+        'school_id': '760305',
+        'fake_username': username,
+        'csrftoken': 'WDRjXUpxPkzTw2CHziVIDDmwo3lr3aU4',
+    }
+
+    headers = {
+        'Connection': 'keep-alive',
+        'Cache-Control': 'max-age=0',
+        'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="96", "Google Chrome";v="96"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"Linux"',
+        'Upgrade-Insecure-Requests': '1',
+        'Origin': 'https://my.dnevnik76.ru',
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'User-Agent': 'Mozilla/5.0 (X11; Fedora; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+        'Sec-Fetch-Site': 'same-origin',
+        'Sec-Fetch-Mode': 'navigate',
+        'Sec-Fetch-User': '?1',
+        'Sec-Fetch-Dest': 'document',
+        'Referer': 'https://my.dnevnik76.ru/accounts/login/',
+        'Accept-Language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7',
+    }
+
+    data = {
+        'csrfmiddlewaretoken': 'WDRjXUpxPkzTw2CHziVIDDmwo3lr3aU4',
+        'next': '',
+        'submit': '',
+        'place': '',
+        'username': f'{username}@760305',
+        'school': '760305',
+        'fake_username': username,
+        'password': password,
+    }
+    session = requests.Session()
+    session.post('https://my.dnevnik76.ru/accounts/login/', headers=headers, cookies=cookies, data=data)
+    return session
+
+def parse_subjects(session):
+    response = session.get('https://my.dnevnik76.ru/marks/current/edurng8409/list/')
+    soup = BeautifulSoup(response.text, features='lxml')
+    divs = soup.find_all('div')
+    marks_div = divs[0]
+    for div in divs:
+        if div.get('id') == 'marks':
+            marks_div = div
+    divs = marks_div.find_all('div')
+    subjects = {}
+    for div in divs:
+        if div.get('id') == 'mark-row':
+            lable = div.find('div').text
+            subjects[f'{lable}'] = []
+            spans = div.find_all('span')
+            for span in spans:
+                if 'avg' in span.get('class'):
+                    subjects[f'{lable}'].append(span.get('title'))
+                elif 'm5' in span.get('class'):
+                    subjects[f'{lable}'].append(5)
+                elif 'm4' in span.get('class'):
+                    subjects[f'{lable}'].append(4)
+                elif 'm3' in span.get('class'):
+                    subjects[f'{lable}'].append(3)
+                elif 'm2' in span.get('class'):
+                    subjects[f'{lable}'].append(2)
+                elif 'm1' in span.get('class'):
+                    subjects[f'{lable}'].append(1)
+                elif 'm-1' in span.get('class'):
+                    subjects[f'{lable}'].append('Н')
+    return subjects
+
+if __name__ == '__main__':
+    main()
---
+++ telegram_main.py
@@ -0,0 +1,42 @@
+import sqlite3
+import telebot
+from main import *
+
+bot = telebot.TeleBot("5099099475:AAHNWwVNOoPR6oPELEntSy3UMlv8y2EZTTI", parse_mode='HTML')
+conn = sqlite3.connect('db', check_same_thread=False)
+cursor = conn.cursor()
+user = {}
+
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    bot.send_message(message.chat.id, 'Добро пожаловать')
+
+
+@bot.message_handler(commands=['settings'])
+def start_message(message):
+    msg = bot.send_message(message.chat.id, 'Введите логин')
+    bot.register_next_step_handler(msg, get_login)
+
+def get_login(message):
+    user['username'] = message.text
+    msg1 = bot.send_message(message.chat.id, 'Введите пароль')
+    bot.register_next_step_handler(msg1, get_password)
+
+def get_password(message):
+    user['password'] = message.text
+    add_user_to_db(message.from_user.id, ''+user['username'], user['password'])
+
+def add_user_to_db(user_id: int, username: str, password: str):
+    cursor.execute('INSERT INTO users (user_id, username, password) VALUES (?, ?, ?)',
+                   (user_id, username, password))
+    conn.commit()
+@bot.message_handler(commands=['marks'])
+def start_message(message):
+    cursor.execute('SELECT username, password FROM users WHERE user_id = ?', (message.from_user.id, ))
+    rows = cursor.fetchall()
+    username, password = '',''
+    for row in rows:
+        username, password = row[0], row[1]
+    bot.send_message(message.chat.id, f'<pre>{get_table(parse_subjects(login(username=username, password=password)))}</pre>')
+    
+bot.polling(none_stop=True, interval=0)
